f0106a5361a45508a3372b90b14c64e41994c505
BESTA\BB10004 <jinxli2021@gmail.com.tw>
2022-04-29 18:20:12 +0800

    Add card caculation by month

--- controller/monthController.js
+++ controller/monthController.js
@@ -1,16 +1,177 @@
 const axios = require("../config/axios");
+const moment = require("moment");
+const { calendarFormat } = require("moment");
+const { type } = require("express/lib/response");
 
-const createCard = async function(req, res, next) {
+/**
+ * 
+ * @param {
+ * updateCard,
+ * createCard,
+ * createCustomField,
+ * createList,
+ * createBoard,
+ * commentCard,
+ * addMemberToCard,
+ * addMemberToBoard,
+ * addAttachmentToCard,
+ * addChecklistToCard,
+ * updateCheckItemStateOnCard,
+ * updateCustomFieldItem,
+ * unconfirmedBoardInvitation,
+ * removeMemberFromCard,
+ * deleteCard} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 
+
+
+
+const createCard = async function (req, res, next) {
     let result = await axios();
-    // console.log(result)
-    res.json(result);
- 
+    let actioins = result.actions;
+
+    res.json(caculation("createCard", actioins));
+
 }
 
+const updateCard = async function (req, res, next) {
+    let result = await axios();
+    let actioins = result.actions;
+    // let count = caculation("updateCard", actioins);
 
+    res.json(caculationById("updateCard", actioins, req.params.id));
 
+}
+const deleteCard = async function (req, res, next) {
+    let result = await axios();
+    let actioins = result.actions;
+
+    res.json(caculation("deleteCard", actioins));
+
+}
+
+const date = async function (req, res, next) {
+    let actioin = "createCard";
+    let result = await axios();
+    let actioins = result.actions;
+    let allDate = [];
+
+    for (var i = 0; i < actioins.length; i++) {
+        if (actioins[i].type == actioin) {
+            month = moment(actioins[i].date);
+            allDate.push(month);
+        }
+
+    }
+    res.json(allDate);
+
+}
+
+const card = async function (req, res, next) {
+    let re = req.params.id;
+    console.log(re + " " + typeof (re))
+
+    let result = await axios();
+    let actioins = result.actions;
+    let allData = [];
+
+    for (var action of actioins) {
+        // if (card.type == "deleteCard") {
+        //     allData.push(card);
+        //     console.log(card.id + " " + typeof (card.id))
+        // }
+
+        if (action.data.card) {
+            if (action.data.card.id == re) allData.push(action);
+        }
+    }
+    // for(index of actioins){
+    //       Object.keys(index.data.card).forEach(function(key){
+    //     console.log(key)
+    // })
+
+    res.json(allData);
+
+}
+
+function caculation(actioin, data) {
+    let monthCount = [];
+    let count = 0;
+    let month, lastMonth;
+    let key;
+
+    for (var i = 0; i < data.length; i++) {
+        month = moment(data[i].date);
+        if (i == 0) {
+            lastMonth = month;
+        }
+        if (data[i].type == actioin) {
+            if (month.isSame(lastMonth, "month")) {
+                count++;
+                // console.log(month.format("YYYY-MM") + " " + count)
+
+            } else {
+                key = month.format("YYYY-MM");
+                // console.log(lastMonth.format("YYYY-MM") + " con:" + count)
+                monthCount.push({ YYMM: key, count });
+                count = 1;
+                lastMonth = month;
+            }
+        }
+        if (i == data.length - 1) {
+            key = lastMonth.format("YYYY-MM");
+            // console.log(lastMonth.format("YYYY-MM") + " con:" + count)
+            monthCount.push({ YYMM: key, count });
+        }
+    }
+    return monthCount
+}
+
+function caculationById(actioin, dataArr, id) {
+    let monthCount = [];
+    let count = 0;
+    let month, lastMonth;
+    let key;
+
+    for (var i = 0; i < dataArr.length; i++) {
+        month = moment(dataArr[i].date);
+        if (i == 0) {
+            lastMonth = month;
+        }
+
+        if (dataArr[i].data.card) {
+            if (dataArr[i].data.card.id == id){
+                if (dataArr[i].type == actioin) {
+                    if (month.isSame(lastMonth, "month")) {
+                        count++;
+                        // console.log(month.format("YYYY-MM") + " " + count)
+
+                    } else {
+                        key = month.format("YYYY-MM");
+                        // console.log(lastMonth.format("YYYY-MM") + " con:" + count)
+                        monthCount.push({ YYMM: key, count });
+                        count = 1;
+                        lastMonth = month;
+                    }
+                }
+        }
+    }
+
+    if (i == dataArr.length - 1) {
+        key = lastMonth.format("YYYY-MM");
+        // console.log(lastMonth.format("YYYY-MM") + " con:" + count)
+        monthCount.push({ YYMM: key, count });
+    }
+}
+return monthCount
+}
 
 module.exports = {
-    createCard
+    createCard,
+    updateCard,
+    deleteCard,
+    card,
+    date
 }
--- routes/count.js
+++ routes/count.js
@@ -4,5 +4,8 @@ const monthController = require("../controller/monthController");
 
 
 router.get("/month/createCard", monthController.createCard);
-
+router.get("/month/updateCard/:id", monthController.updateCard);
+router.get("/month/deleteCard", monthController.deleteCard);
+router.get("/month/date", monthController.date);
+router.get("/month/card/:id", monthController.card);
 module.exports = router;

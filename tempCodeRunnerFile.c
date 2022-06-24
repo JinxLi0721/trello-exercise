f4faa2cfa7170a91b3e261cd83353c2cda78ea8b
LAPTOP-FPJN483A\n3314 <cathy70477@yahoo.com.tw>
2022-06-25 00:13:08 +0800

    style: coding style formate

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main(){
+//宣告開始跟結束 
+int s,e;
+
+printf("請輸入起始值：");
+scanf("%d",&s);
+printf("請輸入結束值：");
+scanf("%d",&e);
+//祥祥哥資訊 轉載請標註來源  
+// for(int i=s;i<=e;i++){
+// //輸出字元轉ASCII碼 
+// printf("ASCII %d 轉字元為 %c\n",i,i);
+// }
+return 0;
+}

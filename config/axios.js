ced3a30d4c78611a0d5fdc01fe124fe17633e70a
Kos Huang <koshuang@gmail.com>
2022-08-27 17:23:13 +0800

    chore(config): [axios] improve logging

--- config/axios.js
+++ config/axios.js
@@ -1,21 +1,23 @@
 const axios = require("axios");
+const debug = require('debug');
+const debugLog = debug('trello:axios');
+const errorLog = debug('trello:axios:error');
 
 /**
  * Objective: make this file as a simple axios client
  */
 
-let axiosGet = async (url) => {
-  let response =  await axios.get(url)
-    .then(res => {
-      console.log(`statusCode: ${res.status}`)
-      return res.data;
-    })
-    .catch(error => {
-      console.error('Request error', error);
-      throw error;
-    });
+const axiosGet = async (url) => {
+  try {
+    const response =  await axios.get(url);
+    debugLog(`statusCode: ${response.status}`);
 
-  return response;
+    return response.data;
+  } catch (error) {
+    errorLog('Request error', error);
+
+    throw error;
+  }
 };
 
 module.exports = axiosGet;

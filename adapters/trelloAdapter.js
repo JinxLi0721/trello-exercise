c65c176d7933f37a4b8c84feae625203bbca3219
Kos Huang <koshuang@gmail.com>
2022-08-27 18:55:46 +0800

    chore(adapter): [TrelloAdapter] support params argument for getBoard()

--- adapters/trelloAdapter.js
+++ adapters/trelloAdapter.js
@@ -8,8 +8,31 @@ class TrelloAdaptor {
     this.boardId = options.boardId;
   }
 
-  async getBoard() {
-    const url = `https://api.trello.com/1/boards/${this.boardId}?key=${this.key}&token=${this.token}&fields=all&actions=all&action_fields=all&actions_limit=1000&cards=all&card_fields=all&card_attachments=true&labels=all&lists=all&list_fields=all&members=all&member_fields=all&checklists=all&checklist_fields=all&organization=false`;
+  async getBoard(params) {
+    let parameters = {
+      key: this.key,
+      token: this.token,
+      fields: "all",
+      actions: "all",
+      action_fields: "all",
+      actions_limit: 1000,
+      cards: "all",
+      card_fields: "all",
+      card_attachments: true,
+      labels: "all",
+      lists: "all",
+      list_fields: "all",
+      members: "all",
+      member_fields: "all",
+      checklists: "all",
+      checklist_fields: "all",
+      organization: false,
+      ...params,
+    };
+
+    const querystring = new URLSearchParams(parameters).toString();
+    const url = `https://api.trello.com/1/boards/${this.boardId}?${querystring}`;
+
     return await axiosClient.get(url);
   }
 }

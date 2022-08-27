ecbcbd77fc3648c5d6ac94c9d76712fe918b4253
Kos Huang <koshuang@gmail.com>
2022-08-27 17:47:17 +0800

    refactor(config): [axios] extract class AxiosClient

--- adapters/trelloAdapter.js
+++ adapters/trelloAdapter.js
@@ -1,4 +1,4 @@
-const axiosGet = require("../config/axios");
+const axiosClient = require("../lib/axiosClient");
 
 class TrelloAdaptor {
   constructor(boardId) {
@@ -7,7 +7,7 @@ class TrelloAdaptor {
 
   async getBoard() {
     const url = `https://api.trello.com/1/boards/${this.boardId}?key=e4ca0224f7ed7ba9dceac38b122ef10e&token=22dc795b770cc5fe1a66ee042ebd69caec1a8276917963e6ececd5ac20263ccd&fields=all&actions=all&action_fields=all&actions_limit=1000&cards=all&card_fields=all&card_attachments=true&labels=all&lists=all&list_fields=all&members=all&member_fields=all&checklists=all&checklist_fields=all&organization=false`;
-    return await axiosGet(url);
+    return await axiosClient.get(url);
   }
 }
 
